bf818d0a94653d58d3ff1791021537af4d81ded3
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-10-16 17:02:03 +0200

    no message

--- login.Designer.cs
+++ login.Designer.cs
@@ -50,8 +50,9 @@
             this.panel1.Controls.Add(this.btn_exit);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1126, 40);
+            this.panel1.Size = new System.Drawing.Size(1001, 32);
             this.panel1.TabIndex = 1;
             // 
             // btn_minimize
@@ -62,10 +63,10 @@
             this.btn_minimize.FlatAppearance.BorderSize = 0;
             this.btn_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_minimize.Location = new System.Drawing.Point(993, 0);
+            this.btn_minimize.Location = new System.Drawing.Point(883, 0);
             this.btn_minimize.Margin = new System.Windows.Forms.Padding(0);
             this.btn_minimize.Name = "btn_minimize";
-            this.btn_minimize.Size = new System.Drawing.Size(66, 40);
+            this.btn_minimize.Size = new System.Drawing.Size(59, 32);
             this.btn_minimize.TabIndex = 2;
             this.btn_minimize.Text = "-";
             this.btn_minimize.UseVisualStyleBackColor = false;
@@ -80,10 +81,10 @@
             this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_exit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btn_exit.Location = new System.Drawing.Point(1057, 0);
+            this.btn_exit.Location = new System.Drawing.Point(940, 0);
             this.btn_exit.Margin = new System.Windows.Forms.Padding(0);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(69, 40);
+            this.btn_exit.Size = new System.Drawing.Size(61, 32);
             this.btn_exit.TabIndex = 1;
             this.btn_exit.Text = "X";
             this.btn_exit.UseVisualStyleBackColor = false;
@@ -95,9 +96,9 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(232)))), ((int)(((byte)(31)))));
-            this.label1.Location = new System.Drawing.Point(487, 413);
+            this.label1.Location = new System.Drawing.Point(433, 330);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 29);
+            this.label1.Size = new System.Drawing.Size(110, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username";
             // 
@@ -107,27 +108,29 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(232)))), ((int)(((byte)(31)))));
-            this.label2.Location = new System.Drawing.Point(487, 510);
+            this.label2.Location = new System.Drawing.Point(433, 408);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 29);
+            this.label2.Size = new System.Drawing.Size(106, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password";
             // 
             // txt_username
             // 
-            this.txt_username.Location = new System.Drawing.Point(421, 445);
+            this.txt_username.Location = new System.Drawing.Point(374, 356);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(272, 26);
+            this.txt_username.Size = new System.Drawing.Size(242, 22);
             this.txt_username.TabIndex = 4;
             this.txt_username.TextChanged += new System.EventHandler(this.txt_username_TextChanged);
             // 
             // mtxt_password
             // 
             this.mtxt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtxt_password.Location = new System.Drawing.Point(421, 542);
+            this.mtxt_password.Location = new System.Drawing.Point(374, 434);
+            this.mtxt_password.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.mtxt_password.Name = "mtxt_password";
             this.mtxt_password.PasswordChar = '*';
-            this.mtxt_password.Size = new System.Drawing.Size(272, 26);
+            this.mtxt_password.Size = new System.Drawing.Size(242, 23);
             this.mtxt_password.TabIndex = 5;
             this.mtxt_password.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             this.mtxt_password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mtxt_password_KeyDown);
@@ -135,10 +138,12 @@
             // pic_logo
             // 
             this.pic_logo.BackColor = System.Drawing.Color.Transparent;
+            this.pic_logo.Image = global::ProvaClasse.Properties.Resources.logo_login;
             this.pic_logo.InitialImage = ((System.Drawing.Image)(resources.GetObject("pic_logo.InitialImage")));
-            this.pic_logo.Location = new System.Drawing.Point(396, 67);
+            this.pic_logo.Location = new System.Drawing.Point(352, 54);
+            this.pic_logo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pic_logo.Name = "pic_logo";
-            this.pic_logo.Size = new System.Drawing.Size(313, 324);
+            this.pic_logo.Size = new System.Drawing.Size(278, 259);
             this.pic_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pic_logo.TabIndex = 6;
             this.pic_logo.TabStop = false;
@@ -146,9 +151,10 @@
             // btn_login
             // 
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_login.Location = new System.Drawing.Point(476, 610);
+            this.btn_login.Location = new System.Drawing.Point(423, 488);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(178, 41);
+            this.btn_login.Size = new System.Drawing.Size(158, 33);
             this.btn_login.TabIndex = 7;
             this.btn_login.Text = "Inicia sessió";
             this.btn_login.UseVisualStyleBackColor = true;
@@ -156,10 +162,11 @@
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1126, 723);
+            this.BackgroundImage = global::ProvaClasse.Properties.Resources.bg_image;
+            this.ClientSize = new System.Drawing.Size(1001, 578);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.pic_logo);
             this.Controls.Add(this.mtxt_password);
@@ -168,6 +175,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "login";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
