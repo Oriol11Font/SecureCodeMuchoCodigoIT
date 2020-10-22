d8524aef522a472e8b26d67440b4c3144f5eaa70
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-10-22 17:41:04 +0200

    buttone

--- LibreriaControles/AppExecute.Designer.cs
+++ LibreriaControles/AppExecute.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hola";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // AppExecute
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Name = "AppExecute";
+            this.Size = new System.Drawing.Size(526, 277);
             this.Load += new System.EventHandler(this.AppExecute_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- LibreriaControles/AppExecute.cs
+++ LibreriaControles/AppExecute.cs
@@ -21,5 +21,10 @@ namespace LibreriaControles
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
