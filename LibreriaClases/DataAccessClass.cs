78e23535f9133b8f91ad9178c15e0299902dfc7a
Pau Colomé Diaz <38383754+MuerteBolonesa@users.noreply.github.com>
2020-10-22 18:23:15 +0200

    Creació de la llibreria de Classes

---
+++ LibreriaClases/DataAccessClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LibreriaClases
+{
+    public partial class DataAccessClass
+    {
+
+        public DataAccessClass()
+        {
+            query = "SELECT * FROM dbo.Factories ";
+        }    
+
+        private SqlConnection conn;
+        private string query;
+        private DataSet dts;
+
+        
+
+    }
+}
