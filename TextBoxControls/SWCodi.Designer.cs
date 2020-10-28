aaed1f7010a927dba84a6e9e3e6ddc0a278e65b8
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-10-28 16:36:36 +0100

    commit oriol

---
+++ TextBoxControls/SWCodi.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TextBoxControls
+{
+    partial class SWCodi
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodiBox = new System.Windows.Forms.TextBox();
+            this.DescBox = new System.Windows.Forms.TextBox();
+            this.AttentionRequerit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AttentionRequerit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CodiBox
+            // 
+            this.CodiBox.Location = new System.Drawing.Point(36, 3);
+            this.CodiBox.Name = "CodiBox";
+            this.CodiBox.Size = new System.Drawing.Size(67, 22);
+            this.CodiBox.TabIndex = 0;
+            this.CodiBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ObreCS);
+            this.CodiBox.Leave += new System.EventHandler(this.ValidacioCodi);
+            // 
+            // DescBox
+            // 
+            this.DescBox.Enabled = false;
+            this.DescBox.Location = new System.Drawing.Point(109, 3);
+            this.DescBox.Name = "DescBox";
+            this.DescBox.ReadOnly = true;
+            this.DescBox.Size = new System.Drawing.Size(237, 22);
+            this.DescBox.TabIndex = 1;
+            // 
+            // AttentionRequerit
+            // 
+            this.AttentionRequerit.Image = global::TextBoxControls.Properties.Resources.alertRequisit;
+            this.AttentionRequerit.Location = new System.Drawing.Point(4, 3);
+            this.AttentionRequerit.Name = "AttentionRequerit";
+            this.AttentionRequerit.Size = new System.Drawing.Size(26, 22);
+            this.AttentionRequerit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AttentionRequerit.TabIndex = 2;
+            this.AttentionRequerit.TabStop = false;
+            this.AttentionRequerit.Visible = false;
+            // 
+            // SWCodi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.AttentionRequerit);
+            this.Controls.Add(this.DescBox);
+            this.Controls.Add(this.CodiBox);
+            this.Name = "SWCodi";
+            this.Size = new System.Drawing.Size(349, 28);
+            ((System.ComponentModel.ISupportInitialize)(this.AttentionRequerit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CodiBox;
+        private System.Windows.Forms.TextBox DescBox;
+        private System.Windows.Forms.PictureBox AttentionRequerit;
+    }
+}
---
+++ TextBoxControls/SWCodi.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace TextBoxControls
+{
+    public partial class SWCodi : UserControl
+    {
+        private Boolean Requerit;
+        private String FormCS;
+        private String ClasseCS;
+        private String ControlID;
+        private String NomTaula;
+        private String NomCodi;
+        private String NomDesc = "Salesians Sarria";
+        private String NomId;
+
+        ObjetosClase.swcodiObj taula = new ObjetosClase.swcodiObj();
+
+        public SWCodi()
+        {
+            InitializeComponent();
+        }
+
+        private void ObreCS(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                Form formCs = new Form();
+                formCs.Text = FormCS;
+                formCs.Show();
+            }
+        }
+
+        private void ValidacioCodi(object sender, EventArgs e)
+        {
+            NomTaula = taula.NomTaula();
+            Requerit = true;
+            NomCodi = CodiBox.Text;
+
+            String sql = "Select NomDesc, NomCodi, NomId from " + NomTaula + " where NomCodi=" + NomCodi;
+
+            //portarTaula(sql);
+            /*
+            if (Requerit)
+            {
+                if ()
+                {
+                    CodiBox.BackColor = Color.Red;
+                    AttentionRequerit.Visible = true;
+                }
+                else
+                {
+                    CodiBox.BackColor = Color.White;
+                    AttentionRequerit.Visible = false;
+
+                    if (ControlID == nomCodi)
+                    {
+                        DescBox.Text = nomDesc;
+                    }
+                    else
+                    {
+                        AttentionRequerit.Visible = true;
+                        DescBox.Text = "Unknown data";
+                    }
+                }
+            }
+            else
+            {
+                CodiBox.BackColor = Color.White;
+                AttentionRequerit.Visible = false;
+
+                if (ControlID == nomCodi)
+                {
+                    DescBox.Text = nomDesc;
+                }
+                else
+                {
+                    AttentionRequerit.Visible = true;
+                    DescBox.Text = "Unknown data";
+                }
+            }*/
+        }
+    }
+}
