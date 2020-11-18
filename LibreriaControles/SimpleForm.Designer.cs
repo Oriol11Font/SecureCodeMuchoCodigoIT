4fe4dced506c6c81b8ce083b98f568f02e9a3c4e
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-11-18 16:47:55 +0100

    SimpleForm - Canvis al dataset assignada la taula

--- LibreriaControles/SimpleForm.Designer.cs
+++ LibreriaControles/SimpleForm.Designer.cs
@@ -31,28 +31,31 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btn_actualitzar = new System.Windows.Forms.Button();
             this.btn_datagrid = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(29, 229);
+            this.dataGridView1.Location = new System.Drawing.Point(1194, 140);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(20, 100, 20, 20);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(550, 350);
+            this.dataGridView1.Size = new System.Drawing.Size(700, 400);
             this.dataGridView1.TabIndex = 19;
             // 
             // btn_actualitzar
             // 
             this.btn_actualitzar.ForeColor = System.Drawing.Color.Black;
-            this.btn_actualitzar.Location = new System.Drawing.Point(643, 118);
+            this.btn_actualitzar.Location = new System.Drawing.Point(993, 475);
+            this.btn_actualitzar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_actualitzar.Name = "btn_actualitzar";
-            this.btn_actualitzar.Size = new System.Drawing.Size(160, 30);
+            this.btn_actualitzar.Size = new System.Drawing.Size(144, 24);
             this.btn_actualitzar.TabIndex = 20;
             this.btn_actualitzar.Text = "Actualitzar";
             this.btn_actualitzar.UseVisualStyleBackColor = true;
@@ -60,83 +63,29 @@
             // btn_datagrid
             // 
             this.btn_datagrid.ForeColor = System.Drawing.Color.Black;
-            this.btn_datagrid.Location = new System.Drawing.Point(643, 173);
+            this.btn_datagrid.Location = new System.Drawing.Point(993, 516);
+            this.btn_datagrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_datagrid.Name = "btn_datagrid";
-            this.btn_datagrid.Size = new System.Drawing.Size(160, 30);
+            this.btn_datagrid.Size = new System.Drawing.Size(144, 24);
             this.btn_datagrid.TabIndex = 21;
             this.btn_datagrid.Text = "Veure DataSet";
             this.btn_datagrid.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 120);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 20);
-            this.label1.TabIndex = 22;
-            this.label1.Text = "Camp1 :";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 181);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 20);
-            this.label2.TabIndex = 23;
-            this.label2.Text = "Camp2 :";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(311, 123);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 20);
-            this.label3.TabIndex = 24;
-            this.label3.Text = "Camp3 :";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(311, 181);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 20);
-            this.label4.TabIndex = 25;
-            this.label4.Text = "Camp4 :";
-            // 
-            // swTextBox1
-            // 
-            // 
-            // swTextBox2
-            // 
-            // 
-            // swTextBox3
-            // 
-            // 
-            // swTextBox4
-            // 
-            // 
             // SimpleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 606);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1923, 1102);
             this.Controls.Add(this.btn_datagrid);
             this.Controls.Add(this.btn_actualitzar);
             this.Controls.Add(this.dataGridView1);
             this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(3);
             this.Name = "SimpleForm";
             this.Text = "Form2";
             this.Controls.SetChildIndex(this.dataGridView1, 0);
             this.Controls.SetChildIndex(this.btn_actualitzar, 0);
             this.Controls.SetChildIndex(this.btn_datagrid, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.label3, 0);
-            this.Controls.SetChildIndex(this.label4, 0);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -144,14 +93,10 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.ListBox listColumns;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btn_actualitzar;
         private System.Windows.Forms.Button btn_datagrid;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
 
     }
 }
--- LibreriaControles/SimpleForm.cs
+++ LibreriaControles/SimpleForm.cs
@@ -1,20 +1,41 @@
-﻿using System;
+﻿using LibreriaClases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LibreriaControles
 {
     public partial class SimpleForm : BaseForm
     {
+        private DataAccessClass dtb = new DataAccessClass();
+        private String _searchText;
+        private DataTable _dt;
+        private DataTable _formattedDt;
+
         public SimpleForm()
         {
             InitializeComponent();
+            DataSet ds = dtb.GetTable("Agencies");
+            SetData(ds);
+            ApplyStyle();
+            DoubleBuffered = true;
         }
+
+        private void SetData(DataSet ds)
+        {
+            _dt = ds.Tables[0];
+            _formattedDt = _dt;
+            dataGridView1.DataSource = _dt;
+        }
+
+        private void ApplyStyle()
+        {
+            dataGridView1.ForeColor = Color.Black;
+        }
+
+
     }
 }
