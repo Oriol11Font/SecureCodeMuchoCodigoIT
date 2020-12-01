d472d5ea2af7eeae62863d7389eb6ef0efd04791
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-12-01 12:27:47 +0100

    me deixat una cosa

--- LibreriaControles/Properties/Resources.Designer.cs
+++ LibreriaControles/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BasicForms.Properties {
+namespace LibreriaControles.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BasicForms.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BasicForms.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibreriaControles.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,16 @@ namespace BasicForms.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Guti {
+            get {
+                object obj = ResourceManager.GetObject("Guti", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
