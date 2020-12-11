9d7d90c081960cdc4be45541508cc6f4e49c90e5
Oriol Font <Oriol11Font@users.noreply.github.com>
2020-12-11 17:37:45 +0100

    Commit - usercategories

--- Categories/UserCategories.cs
+++ Categories/UserCategories.cs
@@ -11,11 +11,30 @@ using System.Windows.Forms;
 
 namespace Categories
 {
-    public partial class UserCategories : SimpleForm
+    public partial class UserCategories : BaseForm
     {
-        public UserCategories()
+        public UserCategories(String username, String imgProfile)
         {
+            FormTitle = "Categories dels Usuaris";
+            UserName = username;
+            profileImg = imgProfile;
             InitializeComponent();
+        }
+
+        CategoriesEntities db;
+        List<UserCategory> cat;
+
+        private void CarregaDades()
+        {
+            db = new CategoriesEntities();
+
+            cat = db.UserCategories.ToList();
+
+            dtgCategories.DataSource = cat;
+        }
+        private void UserCategories_Load(object sender, EventArgs e)
+        {
+            CarregaDades();
         }
     }
 }
