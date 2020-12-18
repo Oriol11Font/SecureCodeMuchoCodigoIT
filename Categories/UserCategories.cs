989b4ee48f00c30bec8ec3641b0c83c0de870b38
Oriol Font <o.font@ratenow.es>
2020-12-18 16:25:32 +0100

    Dlls arreglades i buttons amb la nova bbdd

--- Categories/UserCategories.cs
+++ Categories/UserCategories.cs
@@ -11,11 +11,6 @@ namespace Categories
     {
         public UserCategories(String username, String imgProfile)
         {
-            SqlConnectionStringBuilder CategoriesEntities = new SqlConnectionStringBuilder();
-
-            //Console.WriteLine(builder.WriteLine);
-            CategoriesEntities.ConnectionString = $"Data Source={Environment.MachineName}\\SQLEXPRESS;Initial Catalog=SecureCore;Integrated Security=SSPI;User Id=secureCoreApplication;Password=test123456789";
-
             FormTitle = "Categories dels Usuaris";
             UserName = username;
             profileImg = imgProfile;
