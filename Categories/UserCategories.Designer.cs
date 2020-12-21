02243a72baaac2d8c7d934763715a5861bc30946
Oriol Font <o.font@ratenow.es>
2020-12-21 17:27:39 +0100

    Categories acabades amb el boto actualitzar

--- Categories/UserCategories.Designer.cs
+++ Categories/UserCategories.Designer.cs
@@ -29,9 +29,9 @@ namespace Categories
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridViewTextBoxColumn16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -58,9 +58,7 @@ namespace Categories
             this.label3 = new System.Windows.Forms.Label();
             this.panel8 = new System.Windows.Forms.Panel();
             this.dtgCategories = new System.Windows.Forms.DataGridView();
-            this.createbtn = new System.Windows.Forms.Button();
             this.btn_actualitzar = new System.Windows.Forms.Button();
-            this.btn_datagrid = new System.Windows.Forms.Button();
             this.backbtn = new System.Windows.Forms.Button();
             this.panel8.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtgCategories)).BeginInit();
@@ -303,9 +301,7 @@ namespace Categories
             // panel8
             // 
             this.panel8.Controls.Add(this.dtgCategories);
-            this.panel8.Controls.Add(this.createbtn);
             this.panel8.Controls.Add(this.btn_actualitzar);
-            this.panel8.Controls.Add(this.btn_datagrid);
             this.panel8.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel8.Location = new System.Drawing.Point(10, 470);
             this.panel8.Name = "panel8";
@@ -315,8 +311,8 @@ namespace Categories
             // dtgCategories
             // 
             this.dtgCategories.AllowUserToAddRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Gainsboro;
-            this.dtgCategories.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Gainsboro;
+            this.dtgCategories.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dtgCategories.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtgCategories.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.dtgCategories.BackgroundColor = System.Drawing.Color.Gainsboro;
@@ -331,58 +327,35 @@ namespace Categories
             this.dtgCategories.Name = "dtgCategories";
             this.dtgCategories.ReadOnly = true;
             this.dtgCategories.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Tomato;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Red;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dtgCategories.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Tomato;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgCategories.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dtgCategories.RowHeadersWidth = 20;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Yellow;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
-            this.dtgCategories.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            this.dtgCategories.RowsDefaultCellStyle = dataGridViewCellStyle6;
             this.dtgCategories.Size = new System.Drawing.Size(1900, 540);
             this.dtgCategories.TabIndex = 27;
-            // 
-            // createbtn
-            // 
-            this.createbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.createbtn.ForeColor = System.Drawing.Color.White;
-            this.createbtn.Location = new System.Drawing.Point(18, 2);
-            this.createbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.createbtn.Name = "createbtn";
-            this.createbtn.Size = new System.Drawing.Size(180, 39);
-            this.createbtn.TabIndex = 26;
-            this.createbtn.Text = "Afegir";
-            this.createbtn.UseVisualStyleBackColor = true;
             // 
             // btn_actualitzar
             // 
             this.btn_actualitzar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_actualitzar.ForeColor = System.Drawing.Color.White;
-            this.btn_actualitzar.Location = new System.Drawing.Point(204, 2);
+            this.btn_actualitzar.Location = new System.Drawing.Point(36, 2);
             this.btn_actualitzar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_actualitzar.Name = "btn_actualitzar";
             this.btn_actualitzar.Size = new System.Drawing.Size(180, 39);
             this.btn_actualitzar.TabIndex = 24;
             this.btn_actualitzar.Text = "Actualitzar";
             this.btn_actualitzar.UseVisualStyleBackColor = true;
-            // 
-            // btn_datagrid
-            // 
-            this.btn_datagrid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_datagrid.ForeColor = System.Drawing.Color.White;
-            this.btn_datagrid.Location = new System.Drawing.Point(390, 2);
-            this.btn_datagrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_datagrid.Name = "btn_datagrid";
-            this.btn_datagrid.Size = new System.Drawing.Size(180, 39);
-            this.btn_datagrid.TabIndex = 25;
-            this.btn_datagrid.Text = "Veure DataSet";
-            this.btn_datagrid.UseVisualStyleBackColor = true;
+            this.btn_actualitzar.Click += new System.EventHandler(this.btn_actualitzar_Click);
             // 
             // backbtn
             // 
@@ -456,9 +429,7 @@ namespace Categories
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn17;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn18;
         private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Button createbtn;
         private System.Windows.Forms.Button btn_actualitzar;
-        private System.Windows.Forms.Button btn_datagrid;
         private System.Windows.Forms.Button backbtn;
         private System.Windows.Forms.DataGridView dtgCategories;
     }
