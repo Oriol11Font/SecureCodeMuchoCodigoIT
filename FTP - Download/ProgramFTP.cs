51695e154d2fbd41812d966cc7e0d2da0295f963
Oriol Font <o.font@ratenow.es>
2021-01-15 16:53:33 +0100

    FTP-Download

---
+++ FTP - Download/ProgramFTP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP___Download
+{
+    class ProgramFTP
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            while (input != "exit")
+            {
+                if (input  == "hola")
+                {
+                    Console.WriteLine(input);
+                } else {
+                    Console.WriteLine("Te jodes");
+                }
+
+                input = Console.ReadLine();
+                
+            }
+        }
+    }
+}
