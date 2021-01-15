a039cb2c4778fe4a5a1e63d67a23b41c7564f6fc
polmontolio <72408101+polmontolio@users.noreply.github.com>
2021-01-15 17:05:35 +0100

    oujador ok?

--- FTP/Pujador.Designer.cs
+++ FTP/Pujador.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 506);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Pujador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Pujador";
             this.Text = "Pujador";
+            this.Load += new System.EventHandler(this.Pujador_Load);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- FTP/Pujador.cs
+++ FTP/Pujador.cs
@@ -1,20 +1,58 @@
-﻿using System;
+﻿using BasicForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FTP
 {
-    public partial class Pujador : Form
+    public partial class Pujador : BaseForm
     {
         public Pujador()
         {
             InitializeComponent();
+        }
+
+        private void Pujador_Load(object sender, EventArgs e)
+        {
+            //Set Host, User and Password for the FTP;
+            string ftpHost = "192.168.10.1";
+            string ftpUser = "g4";
+            string ftpPW = "12345aA";
+
+            string ftpFullPath = @"ftp://" + ftpHost;
+
+            try
+            {
+                //Establish a connection to the server;
+                FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(ftpFullPath);
+                ftp.Credentials = new NetworkCredential(ftpUser, ftpPW);
+
+                ftp.KeepAlive = true;
+                ftp.UseBinary = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hola;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("..\\DLL\\FTP - Download");
         }
     }
 }
